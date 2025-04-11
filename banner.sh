9587410319360c19ebbcc681455f44943e1a373f
marchwicki <57815103+marchwicki@users.noreply.github.com>
2025-04-11 11:45:26 -0400

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,5 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+# Wait for the desktop to load
 sleep 10
-conky -c .conkyrc_banner &
+
+# Get the width of the primary display
+SCREEN_WIDTH=$(xrandr | grep '*' | head -n1 | awk '{print $1}' | cut -d'x' -f1)
+
+# Update both conky config files to use the detected width
+sed -i "s/\(minimum_width\s*=\s*\)[0-9]\+/\1$SCREEN_WIDTH/" ~/.conkyrc_banner
+sed -i "s/\(minimum_width\s*=\s*\)[0-9]\+/\1$SCREEN_WIDTH/" ~/.conkyrc_transparent
+
+# Start both conkys
+conky -c ~/.conkyrc_banner &
 sleep 1
-conky -c .conkyrc_transparent &
+conky -c ~/.conkyrc_transparent &
